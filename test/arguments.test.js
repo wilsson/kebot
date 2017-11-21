a6a24c9a247dfbf942d6d255906c8554cbeaf8fd
Juan Pablo <juanpablocs21@gmail.com>
2017-11-21 12:23:24 -0500

    test(kebot test): se remueve execSync y se cambio por exec

--- test/arguments.test.js
+++ test/arguments.test.js
@@ -8,26 +8,30 @@ var path = require('path');
 var bin = path.join(__dirname, './../bin/kebot');
 
 test('argument miss', (done) => {
-    exec(`node ${bin} task:args-miss -t`, (err, stdout)=>{
-        expect(stdout.toString().split('\n').slice(1).join('')).toEqual('execute task');
+    exec(`$(which node) ${bin} task:args-miss -t`, (err, stdout)=>{
+        expect(stdout.split('\n').slice(1).join('')).toEqual('execute task');
         done();
     });
 });
 
-test('argument contain "args" in flag --args', () => {
-    let command = execSync(`$(which node) ${bin} task:args  --args args -t`);
-
-    expect(command.toString().split('\n').slice(1).join('')).toEqual('args');
+test('argument contain "args" in flag --args', (done) => {
+    exec(`$(which node) ${bin} task:args  --args args -t`, (err,stdout)=>{
+        expect(stdout.split('\n').slice(1).join('')).toEqual('args');
+        done();
+    });
 });
 
-test('argument environment in flag --env', () => {
-    let command = execSync(`$(which node) ${bin} task:env  --env production -t`);
-    expect(command.toString().split('\n').slice(1).join('')).toEqual('true');
+test('argument environment in flag --env', (done) => {
+    exec(`$(which node) ${bin} task:env  --env production -t`, (err, stdout)=>{
+        expect(stdout.split('\n').slice(1).join('')).toEqual('true');
+        done();
+    });
 });
 
-test('arguments multiples flags "--email" and "--template"', () => {
-    const command = execSync(`$(which node) ${bin} task:args-multiple  --email juanpablocs21@gmail.com --template register-action -t`);
-    const expected = ['juanpablocs21@gmail.com', 'register-action'];
-
-    expect(command.toString().split('\n').slice(1)).toEqual(expect.arrayContaining(expected));
+test('arguments multiples flags "--email" and "--template"', (done) => {
+    exec(`$(which node) ${bin} task:args-multiple  --email juanpablocs21@gmail.com --template register-action -t`, (err, stdout)=>{
+        const expected = ['juanpablocs21@gmail.com', 'register-action'];
+        expect(stdout.split('\n').slice(1)).toEqual(expect.arrayContaining(expected));
+        done();
+    });
 });
