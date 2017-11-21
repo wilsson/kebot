9c2b29a3c7ad0e185c50abb00360da039638eae0
Juan Pablo <juanpablocs21@gmail.com>
2017-11-21 12:01:03 -0500

    fix(travis test): node error command en travis

--- test/arguments.test.js
+++ test/arguments.test.js
@@ -6,24 +6,24 @@ var path = require('path');
 var bin = path.join(__dirname, './../bin/kebot');
 
 test('argument miss', () => {
-    let command = execSync(`node ${bin} task:args-miss -t`);
+    let command = execSync(`$(which node) ${bin} task:args-miss -t`);
 
     expect(command.toString().split('\n').slice(1).join('')).toEqual('execute task');
 });
 
 test('argument contain "args" in flag --args', () => {
-    let command = execSync(`node ${bin} task:args  --args args -t`);
+    let command = execSync(`$(which node) ${bin} task:args  --args args -t`);
 
     expect(command.toString().split('\n').slice(1).join('')).toEqual('args');
 });
 
 test('argument environment in flag --env', () => {
-    let command = execSync(`node ${bin} task:env  --env production -t`);
+    let command = execSync(`$(which node) ${bin} task:env  --env production -t`);
     expect(command.toString().split('\n').slice(1).join('')).toEqual('true');
 });
 
 test('arguments multiples flags "--email" and "--template"', () => {
-    const command = execSync(`node ${bin} task:args-multiple  --email juanpablocs21@gmail.com --template register-action -t`);
+    const command = execSync(`$(which node) ${bin} task:args-multiple  --email juanpablocs21@gmail.com --template register-action -t`);
     const expected = ['juanpablocs21@gmail.com', 'register-action'];
 
     expect(command.toString().split('\n').slice(1)).toEqual(expect.arrayContaining(expected));
