a85449eb878d033180ea1aaf28f22fdbe5f6426e
Juan Pablo <juanpablocs21@gmail.com>
2017-11-21 14:11:42 -0500

    test(kebot test): parametro shell en exec

--- test/arguments.test.js
+++ test/arguments.test.js
@@ -6,26 +6,29 @@ var execSync = require('child_process').execSync;
 var path = require('path');
 
 var bin = path.join(__dirname, './../bin/kebot');
+var execOption = {
+    shell: true
+};
 
 test('argument miss', (done) => {
     exec(`$(which node) ${bin} task:args-miss -t`, (err, stdout)=>{
         expect(stdout.split('\n').slice(1).join('')).toEqual('execute task');
         done();
-    });
+    },execOption);
 });
 
 test('argument contain "args" in flag --args', (done) => {
     exec(`$(which node) ${bin} task:args  --args args -t`, (err,stdout)=>{
         expect(stdout.split('\n').slice(1).join('')).toEqual('args');
         done();
-    });
+    },execOption);
 });
 
 test('argument environment in flag --env', (done) => {
     exec(`$(which node) ${bin} task:env  --env production -t`, (err, stdout)=>{
         expect(stdout.split('\n').slice(1).join('')).toEqual('true');
         done();
-    });
+    },execOption);
 });
 
 test('arguments multiples flags "--email" and "--template"', (done) => {
@@ -33,5 +36,5 @@ test('arguments multiples flags "--email" and "--template"', (done) => {
         const expected = ['juanpablocs21@gmail.com', 'register-action'];
         expect(stdout.split('\n').slice(1)).toEqual(expect.arrayContaining(expected));
         done();
-    });
+    },execOption);
 });
